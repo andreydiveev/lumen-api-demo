65d7f884e75e4bbccdc8069f788280a6871baf02
Andrey Diveev <andrey.diveev@gmail.com>
2017-12-13 13:55:34 +0300

    disable mail on register

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,12 +13,12 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         // 注册mail
-        $this->app->singleton('mailer', function ($app) {
-            $app->configure('services');
-            $app->configure('mail');
-
-            return $app->loadComponent('mail', 'Illuminate\Mail\MailServiceProvider', 'mailer');
-        });
+        //$this->app->singleton('mailer', function ($app) {
+        //    $app->configure('services');
+        //    $app->configure('mail');
+        //
+        //    return $app->loadComponent('mail', 'Illuminate\Mail\MailServiceProvider', 'mailer');
+        //});
 
         // 处理dingo的findOrFail 问题
         // 或许可以放在  ApiExceptionServiceProvider 这样的地方
